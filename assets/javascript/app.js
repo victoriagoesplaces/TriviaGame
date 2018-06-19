3c68a92a62d054c3b5c47dc0e6f2b7ec8d75c411
Victoria Gonzalez <vmjgr@Victorias-MBP.attlocal.net>
2018-06-18 22:22:25 -0400

    new html/question set up js

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+//---------Variables---------------
+
+
+//Define varible where the quiz will be placed
+var quizBox = $('#quiz');
+
+//Array holding the questions and answers
+var quiz = [{
+    question: "1 . What are the two teams making their debuts in the competition?",
+    answers: ["Panama&Egypt", "Panama&Iceland", "Iceland&Morocco", "Egypt&Morocco"],
+    rightAnswer: "Iceland&Panama"
+}, {
+    question: "2 . Who has won the World Cup more times?",
+    answers: ["Germany", "Argentina", "France", "Brazil"],
+    rightAnswer: "Brazil"
+}, {
+    question: "3 . Where is the World Cup taking place?",
+    answers: ["Russia", "Brazil", "South Africa", "China"],
+    rightAnswer: "Russia"
+}, {
+    question: "4 . What city will host the finals of the Cup?",
+    answers: ["Kaliningrad", "Saint Petersburg", "Moscow", "Ekaterinburg"],
+    rightAnswer: "Moscow"
+}, {
+    question: "5 . How many teams will take part in the competition?",
+    answers: ["38", "46", "52", "32"],
+    rightAnswer: "32"
+}, {
+    question:  "6 . How many out of the 12 World Cup stadiums in Russia were built just for the tournament?",
+    answers: ["12", "7", "9", "11"],
+    rightAnswer: "9"
+}, {
+    question: "7 . What stadium has seating outside to meet FIFA's minimum seating capacity?",
+    answers: ["Kazan Arena", "Luzhniki Stadium", "Saint Petersburg Stadium", "Ekaterinburg Arena"],
+    rightAnswer: "Ekaterinburg Arena"
+}, {
+    question: "8 . Who is the current holder of the World Cup coming to the 2018 tournament?",
+    answers: ["Germany", "Brazil", "Spain", "Argentina"],
+    rightAnswer: "Germany"
+}, {
+    question: "9. What kind of animal is the 2018 World Cup mascot(Zabivaka)?",
+    answers: ["Cat", "Tiger", "Wolf", "Bear"],
+    rightAnswer: "Wolf"
+}, {
+    question: "10. How many FIFA World Cup tournaments have there been being prior to 2018?",
+    answers: ["23", "20", "21", "18"],
+    rightAnswer: "20"
+}];
+
